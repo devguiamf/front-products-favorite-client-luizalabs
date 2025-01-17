a59f4d4db6a2ff08656276ba40b06423f03499cf
Guilherme de Almeida <guilherme.amf18@gmail.com>
2025-01-17 13:39:02 -0300

    feat: auth page

--- src/app/auth/auth.page.ts
+++ src/app/auth/auth.page.ts
@@ -2,10 +2,12 @@ import { Component } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { LoginFormComponent } from "./login-form/login-form.component";
 import { RegisterFormComponent } from "./register-form/register-form.component";
+import { ButtonComponent } from "../common/components/button/button.component";
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'auth-page',
-  imports: [ReactiveFormsModule, LoginFormComponent, RegisterFormComponent],
+  imports: [ReactiveFormsModule, LoginFormComponent, RegisterFormComponent, RouterLink],
   templateUrl: './auth.page.html'
 })
 export class AuthPage {
