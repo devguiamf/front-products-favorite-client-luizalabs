8649c152a8d955cb7ce877ab9678aadcd453222e
Guilherme de Almeida <guilherme.amf18@gmail.com>
2025-01-16 23:54:46 -0300

    feat: add template page and header component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'Home',
+        title: 'Home',
+        loadComponent: () => import('./template/template.component').then(c => c.TemplateComponent)
+    },
+    {
+        path: '',
+        redirectTo: 'Home',
+        pathMatch: 'full'
+    }
+];
---
+++ src/app/template/template.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-template',
+  imports: [],
+  standalone: true,
+  templateUrl: './template.component.html'
+})
+export class TemplateComponent {
+
+}
